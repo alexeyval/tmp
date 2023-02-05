a1ea09d2a736c7cd346658a5ae8a646980d39f19
Alexey Valitov <alexeyvalitov28@gmail.com>
2023-02-05 14:44:56 +0500

    read

--- C.go
+++ C.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 )
 
 func main() {
-	//file, err := os.Open("/home/alexey/Documents/Go/route256" +
-	//	"/Соревнование/now2/problem-b-tests/tests/02")
-	//if err != nil {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-	//reader := bufio.NewReader(file)
-	reader := bufio.NewReader(os.Stdin)
+	file, err := os.Open("/home/alexey/Documents/Go/route256/Соревнование/now2/c.test")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	reader := bufio.NewReader(file)
+	//reader := bufio.NewReader(os.Stdin)
 
 	//var t int // 1 - 1000
 	scanner := bufio.NewScanner(reader)
@@ -22,6 +22,7 @@ func main() {
 
 	for scanner.Scan() {
 		text := scanner.Text()
-		_ = text
+		fmt.Println(text)
 	}
+	fmt.Println("OK")
 }
